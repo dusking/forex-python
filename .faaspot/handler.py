38ad98d6c8ba2b035762adb8df98b3aed986f0b1
Omer Duskin <omerd@gigaspaces.com>
2018-04-20 22:52:13 +0300

    use wrapper

---
+++ .faaspot/handler.py
@@ -0,0 +1,103 @@
+import os
+import json
+import urlparse
+import pycountry
+
+from box import Box
+from forex_python.bitcoin import BtcConverter
+from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+
+from moneyed import Money
+from moneyed.localization import format_money
+
+
+DEFAULT_BASE_CURRENCY_CODE = 'USD'
+
+
+def format_value(value, thousands_separator=None, ndigits=None):
+    if thousands_separator and ndigits:
+        return "{:,.{}f}".format(value, ndigits)
+    elif ndigits:
+        return  "{:,.{}f}".format(value, ndigits)
+    elif thousands_separator:
+        return  "{:,}".format(value)
+    return value
+
+
+def get_currency(args):
+    source_currency_code = args.get('source_currency')
+    target_currency_code = args.get('target_currency')
+    ndigits = args.get('ndigits')
+    thousands_separator = args.get('thousands_separator')    
+    source_currency_code = source_currency_code.upper()
+    print("source_currency_code: {}".format(source_currency_code))
+    print("target_currency_code: {}".format(target_currency_code))
+    response = {'source': source_currency_code}
+    c = CurrencyRates(force_decimal=False)
+    info = c.get_rates(source_currency_code)    
+    info =  {k: format_value(v, thousands_separator, ndigits) for k, v in info.iteritems()}
+    if target_currency_code:
+        if target_currency_code not in info:
+            raise Exception("Unrecognized `{}` currency".format(target_currency_code))
+        target_currency_code = target_currency_code.upper()
+        response['target'] = target_currency_code
+        info = info.get(target_currency_code)
+    response['currency'] = info
+    return response
+
+
+def currency_exchange(args):
+    source_currency_code = args.get('source_currency')
+    target_currency_code = args.get('target_currency')
+    amount = args.get('amount')
+    ndigits = args.get('ndigits')
+    thousands_separator = args.get('thousands_separator')
+    if not amount:
+        # for some reason, args.get('amount', '1') doesn't work..
+        amount = 1
+    print("source_currency_code: {}".format(source_currency_code))
+    print("target_currency_code: {}".format(target_currency_code))
+    print("amount: {}".format(amount))
+    amount = float(amount)
+    c = CurrencyRates(force_decimal=False)
+    value = c.convert(source_currency_code, target_currency_code, amount)   
+    value = format_value(value, thousands_separator, ndigits)
+    return {'source': source_currency_code, 'target': target_currency_code, 'amount': amount, 'value': value}
+
+
+def get_country_currency(args):
+    country_lookup = args.get('country')
+    # lookup (vs get) will perform case insensitively without knowing which key the value may match 
+    # it's instead of: country = pycountry.countries.get(name=name)
+    try:
+        print "Searching for: {}".format(country_lookup)
+        country = pycountry.countries.lookup(country_lookup)
+    except Exception:
+        return {}
+    currency = pycountry.currencies.get(numeric=country.numeric)
+    return {'country_name': country.name, 'country_code': country.alpha_2, 'currency_code': currency.alpha_3, 'currency_name': currency.name}
+
+
+def get_formatted_money(args):
+    currency = args.get('currency')
+    amount = args.get('amount')
+    print ("currency: {}".format(currency))
+    print ("amount: {}".format(amount))
+    price = Money(amount=amount, currency=currency)
+    # format_money(price, locale='en_US')
+    return {'value': format_money(price)}
+
+
+def get_bitcoin_value(event, context):
+    query_params = event.get("query", {})
+    params = Box(query_params)
+    print (params)
+    currency_code = DEFAULT_BASE_CURRENCY_CODE
+    b = BtcConverter(force_decimal=True)
+    price = b.get_latest_price(currency_code)
+    return {currency_code: price}
+
+# print get_currency({'source_currency': 'USD', 'target_currency': 'ILS'})
+# print convert_currency({'source_currency': 'USD', 'target_currency': 'ILS', 'amount': '1'})
+# print get_country_currency({'country': 'Israel'})
---
+++ .faaspot/wrapper.py
@@ -0,0 +1,245 @@
+import os
+import json
+import urlparse
+import requests
+import ipaddress
+import threading
+
+import jsonschema
+from functools import wraps
+from jsonschema import validate
+from jsonschema import Draft4Validator
+from jsonschema.exceptions import ValidationError, SchemaError
+
+import base64
+import hashlib
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+from handler import get_currency, currency_exchange, get_country_currency
+
+
+def unpad(data):
+    return data[0:-ord(data[-1])]
+
+
+def decrypt(hex_data):
+    key = 'WmFKzhC3YRmEU4dPY3hza8HUu7653Gg3'
+    iv = 'ZS9ATh5Wz4jUN895'
+    data = ''.join(map(chr, bytearray.fromhex(hex_data)))
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    data = unpad(aes.decrypt(data))
+    return base64.b64decode(data)
+
+
+def validate_token(token):
+    user_data = ''
+    if not token:
+        return {'error': "missing user token"}
+    try:
+        user_data = decrypt(token.replace('Basic ', ''))
+        print ("user_data: {0}".format(user_data))
+    except ValueError:
+        return {'error': '`{}` does not appear to be a valid token'.format(token)}
+    return {'user_data': user_data}
+
+
+def callPost(body):    
+    print ('going to send `google-bigquery` request.. body: {}'.format(body))
+    headers = {"Content-Type":"application/json", "Token":"Basic 62646018047677d2f204ffae7dac388bc4cb227d963b729d"}    
+    send_message_url = 'https://us-central1-faaspotit.cloudfunctions.net/google-bigquery'
+    requests.post(send_message_url, data=json.dumps(body), headers=headers)
+
+
+def update_usage(user_id, user_ip_addr, function_id, function_name):
+    body = {"user_id": user_id, 'source_ip': user_ip_addr, 'function_id': function_id, 'function_name': function_name}
+    th = threading.Thread(target=callPost, args=[body])
+    th.daemon = True
+    th.start()
+
+
+def endpoint(schema=None):
+    def endpoint_wrapper(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            event, context = args
+            print ("event: {0}".format(event))
+            print ("context: {0}".format(context))
+            body = event.get("body", "{}")
+            
+            try:
+                body = json.loads(body)
+            except ValueError:
+                body = urlparse.parse_qs(body)  
+                body = {k: v[0] for k, v in body.iteritems()}
+
+            if schema:
+                try:
+                    validate(body, schema)
+                except (ValidationError, SchemaError) as ex:
+                    element = ex.path[0]
+                    message = ex.message
+                    err_message = '{} - {}'.format(element, message)
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps({'error': err_message}),
+                        'headers': {"Content-Type": "application/json"}
+                    }
+                
+            headers = event.get("headers", {})        
+            token = headers.get('Token')    
+            user_ip = headers.get('X-Forwarded-For')
+
+            result = validate_token(token)
+            err = result.get('error')
+            if err:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': err}),
+                    'headers': {"Content-Type": "application/json"}
+                }
+            user_data = result.get('user_data')
+            user_id = user_data.split(':')[0]
+            function_name = event.get('uri').split('/')[-1]
+            function_id = context.get('functionId')
+            update_usage(user_id, user_ip, function_id, function_name)
+            
+            try:
+                response = json.dumps(func(body))    
+            except Exception as ex:
+                return {
+                        'statusCode': 400,
+                        'body': json.dumps({'error': str(ex)}),
+                        'headers': {"Content-Type": "application/json"}
+                    }
+            return {
+                'statusCode': 200,
+                'body': response,
+                'headers': {"Content-Type": "application/json"}
+            }
+        return wrapper
+    return endpoint_wrapper
+
+
+get_currency_schema = {
+    'required': ['source_currency'],
+    'properties':  {
+        'ndigits': {'type': 'integer'},
+        'source_currency': {'type': 'string'},
+        'target_currency': {'type': 'string'}
+    }}
+
+@endpoint(get_currency_schema)
+def wrapper_get_currency(*args, **kwargs):
+    return get_currency(*args, **kwargs)
+
+
+# def wrapper_old(event, context):
+#     print ("event: {0}".format(event))
+#     print ("context: {0}".format(context))
+#     body = event.get("body", "{}")
+#     try:
+#         body = json.loads(body)
+#     except ValueError:
+#         body = urlparse.parse_qs(body)  
+#         body = {k: v[0] for k, v in body.iteritems()}
+#     headers = event.get("headers", {})        
+#     token = headers.get('Token')    
+#     user_ip = headers.get('X-Forwarded-For')
+
+#     result = validate_token(token)
+#     err = result.get('error')
+#     if err:
+#         return {
+#             'statusCode': 400,
+#             'body': json.dumps({'error': err}),
+#             'headers': {"Content-Type": "application/json"}
+#         }
+#     user_data = result.get('user_data')
+#     user_id = user_data.split(':')[0]
+#     function_name = event.get('uri').split('/')[-1]
+#     function_id = context.get('functionId')
+#     update_usage(user_id, user_ip, function_id, function_name)
+    
+#     response = json.dumps(get_currency(body))    
+#     return {
+#         'statusCode': 200,
+#         'body': response,
+#         'headers': {"Content-Type": "application/json"}
+#     }
+
+
+def wrapper_exchange(event, context):
+    print ("event: {0}".format(event))
+    print ("context: {0}".format(context))
+    body = event.get("body", "{}")
+    try:
+        body = json.loads(body)
+    except ValueError:
+        body = urlparse.parse_qs(body)  
+        body = {k: v[0] for k, v in body.iteritems()}
+    headers = event.get("headers", {})        
+    token = headers.get('Token')    
+    user_ip = headers.get('X-Forwarded-For')
+
+    result = validate_token(token)
+    err = result.get('error')
+    if err:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': err}),
+            'headers': {"Content-Type": "application/json"}
+        }
+    user_data = result.get('user_data')
+    user_id = user_data.split(':')[0]
+    function_name = event.get('uri').split('/')[-1]
+    function_id = context.get('functionId')
+    update_usage(user_id, user_ip, function_id, function_name)
+    
+    response = json.dumps(currency_exchange(body))    
+    return {
+        'statusCode': 200,
+        'body': response,
+        'headers': {"Content-Type": "application/json"}
+    }
+
+
+def wrapper_country_currency(event, context):
+    print ("event: {0}".format(event))
+    print ("context: {0}".format(context))
+    body = event.get("body", "{}")
+    try:
+        body = json.loads(body)
+    except ValueError:
+        body = urlparse.parse_qs(body)  
+        body = {k: v[0] for k, v in body.iteritems()}
+    headers = event.get("headers", {})        
+    token = headers.get('Token')    
+    user_ip = headers.get('X-Forwarded-For')
+
+    result = validate_token(token)
+    err = result.get('error')
+    if err:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': err}),
+            'headers': {"Content-Type": "application/json"}
+        }
+    user_data = result.get('user_data')
+    user_id = user_data.split(':')[0]
+    function_name = event.get('uri').split('/')[-1]
+    function_id = context.get('functionId')
+    update_usage(user_id, user_ip, function_id, function_name)
+    
+    response = json.dumps(get_country_currency(body))    
+    return {
+        'statusCode': 200,
+        'body': response,
+        'headers': {"Content-Type": "application/json"}
+    }
+
+
+# event = {u'body': u'{"source_currency":"usd","target_currency":"ils","ndigits":"2"}', u'uri': u'https://app-2bc38fc4-add-demo-execute-function1.spotinst.io/get_currency', u'headers': {u'Content-Length': u'63', u'Request-Source': u'external', u'X-Forwarded-Port': u'443', u'X-Forwarded-For': u'52.6.120.182', u'Host': u'app-2bc38fc4-add-demo-execute-function1.spotinst.io', u'Accept': u'application/json', u'User-Agent': u'curl/7.35.0', u'X-Real-IP': u'52.6.120.182', u'Token': u'Basic 1664d39eaa6eac4ac31cde6e5dd7f217bb528cfc9edb673883f061394076d934', u'X-Forwarded-Proto': u'https', u'X-Custom-ReferrerB': u'52.6.120.182', u'X-Custom-ReferrerA': u'52.6.120.182', u'Content-Type': u'application/json'}, u'query': {}}
+# context = {u'environmentId': u'env-b6436fc5', u'functionVersion': 10, u'timeoutInMillis': 30000, u'functionName': u'fx-45886253', u'memoryLimitInMB': 128, u'functionId': u'fx-45886253', u'invocationId': u'invoc-f6aa0e4d9818'}
+# print wrapper_get_currency(event, context)
